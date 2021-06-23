e2759cb3e33139fe13387a93d968411fe51ea136
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-06-24 01:47:18 +0900

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,16 @@
+#PIP Module(모듈)
+import sys
+import urllib3
+from vmware.vapi.vsphere.client import create_vsphere_client
+
+# Connect My VM Server
+
+session = requests.session
+
+session.verify = False
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+vsphere_client = create_vsphere_client(server_ip = '<ip-address>', username = '<username>', password = '<user_password>', session = session)
+
+vsphere_client.vcenter.VM.list()
